f260a994b43754103b77184afb0ee5804e34cc33
RazorHollow <rob@razorhollow.com>
2022-05-31 16:22:12 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+const input = document.querySelector('#to-do-input')
+const toDoButton = document.querySelector('#submit-button')
+const list = document.querySelector('#todo-list')
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
